3b90c082e5867808c16d7e7d26657dd8a51e15ea
fr-daria <frantsuzova00000@yandex.ru>
2023-02-09 20:51:40 +0300

    Исправление дз(без src/ папки)

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        SalesManager sm = new SalesManager(new long[5]);
+        SalesManager sm = new SalesManager(new long[]{40, 20, 5, 76, 12});
         sm.max(9);
-        sm.selling();
+        System.out.println(sm.selling());
 
     }
 }
--- SalesManager.java
+++ SalesManager.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public class SalesManager {
     protected long[] sales;
 
@@ -19,8 +22,13 @@ public class SalesManager {
     public long selling() {
         long sum = 0;
         for (long j = 0; j < sales.length; j++) {
+            if (Objects.equals(sales[(int) j], Arrays.stream(sales).min())) {
+                continue;
+            } else if (Objects.equals(sales[(int) j], Arrays.stream(sales).max())) {
+                continue;
+            }
             sum = sum + sales[(int) j];
-            sum = sum / sales.length;
+            sum = sum / (sales.length - 2);
         }
         return sum;
     }
